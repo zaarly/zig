cf807b1357e334c2ca7569351e409ffa585e164c
Kameron Ahler <kamahler@gmail.com>
2019-06-24 14:57:31 -0600

    es6

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,11 +2,10 @@
 
 // layout grid overlays
 (function() {
-	var layoutGridOverlaySwitch = document.getElementById('layouts-grid-on-off');
-	var layouts = document.getElementById('layouts');
-
-	function addLayoutGridToggleListeners() {
-		layoutGridOverlaySwitch.addEventListener('change', function() {
+	const layoutGridOverlaySwitch = document.getElementById('layouts-grid-on-off');
+	const layouts = document.getElementById('layouts');
+	const addLayoutGridToggleListeners = () => {
+		layoutGridOverlaySwitch.addEventListener('change', () => {
 			layouts.classList.toggle('js-active');
 		});
 	}
