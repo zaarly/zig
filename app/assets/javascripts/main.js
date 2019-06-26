a4ac5401e36599fd88955790a82bfc4e5b7d3985
Kameron Ahler <kamahler@gmail.com>
2019-06-25 18:56:17 -0600

    add mobile functionality and grid layouts for standard layout up to 1200

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,14 +1,41 @@
-// demo
+// // demo
 
-// layout grid overlays
+// // layout grid overlays
+// (function() {
+// 	const layoutGridOverlaySwitch = document.getElementById('layouts-grid-on-off');
+// 	const layouts = document.getElementById('layouts');
+// 	const addLayoutGridToggleListeners = () => {
+// 		layoutGridOverlaySwitch.addEventListener('change', () => {
+// 			layouts.classList.toggle('js-active');
+// 		});
+// 	}
+
+// 	addLayoutGridToggleListeners();
+// })();
+
+// site nav mobile toggle
 (function() {
-	const layoutGridOverlaySwitch = document.getElementById('layouts-grid-on-off');
-	const layouts = document.getElementById('layouts');
-	const addLayoutGridToggleListeners = () => {
-		layoutGridOverlaySwitch.addEventListener('change', () => {
-			layouts.classList.toggle('js-active');
-		});
-	}
+	const siteNavOpener = document.getElementById('site-nav-open');
+	const siteNavCloser = document.getElementById('site-nav-close');
+	const siteLayoutContainer = document.getElementById('site-layout');
+	const addMobileNavListener = (el) => {
+		if (el == siteNavCloser) {
+			el.addEventListener('click', () => {
+				siteNavOpener.setAttribute('aria-expanded', false);
+				siteNavCloser.setAttribute('aria-expanded', false);
+				siteNavOpener.enabled;
+				siteLayoutContainer.classList.remove('js-site-nav-active');
+			})
+		} else if (el == siteNavOpener) {
+			el.addEventListener('click', () => {
+				siteNavOpener.setAttribute('aria-expanded', true);
+				siteNavCloser.setAttribute('aria-expanded', true);
+				siteNavOpener.disabled;
+				siteLayoutContainer.classList.add('js-site-nav-active');
+			})
+		}
+	};
 
-	addLayoutGridToggleListeners();
+	addMobileNavListener(siteNavOpener);
+	addMobileNavListener(siteNavCloser);
 })();
